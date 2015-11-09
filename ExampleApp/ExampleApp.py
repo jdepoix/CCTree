803cd9f7310fbe9113903094328d5ed811df1e23
Jonas Depoix <bobby69brown69@googlemail.com>
2015-11-09 11:05:05 +0100

    CCTreeDemonstrator module added

--- ExampleApp/ExampleApp.py
+++ ExampleApp/ExampleApp.py
@@ -1,69 +1,21 @@
-import random
-import sys
-
 __author__ = 'Jonas'
 
+import random
+
 from CCTree.CCTree import CCTree
-
-
-def print_cctree_leafs(cctree):
-    """
-    print all leafs of a given CCTree to the console
-
-    :param cctree: CCTree
-    """
-    leafs = cctree.root.get_leafs()
-    line_height = max([len(node_data.data) for node_data in leafs])
-    line = ''
-
-    for j in range(line_height):
-        for i in range(len(leafs)):
-            line_width = max([len(node_data.__str__()) for node_data in leafs[i].data])
-            if j < len(leafs[i].data):
-                new_str = leafs[i].data[j].__str__()
-                if len(new_str) < line_width:
-                    new_str += ' ' * (line_width - len(new_str))
-                line += new_str
-            else:
-                line += ' ' * line_width
-            line += '\t\t'
-
-        print(line)
-        line = ''
-
-
-def build_cctree_interactive(cctree):
-    """
-    goes through the process of clustering with a CCTree interactively, by printing out each iteration
-
-    :param cctree: CCTree
-    """
-    print('-------------------------------------------- INPUT ---------------------------------------------')
-    print_cctree_leafs(cctree)
-
-    while cctree.split_nodes():
-        do = input("press 'n' for next iteration, press any other key to cancel\n")
-        if do == 'n':
-            print('\n---------------------------------------- NEXT ITERATION ----------------------------------------')
-            print_cctree_leafs(cctree)
-        else:
-            print('cancelled!')
-            sys.exit(0)
-
-    print('cluster finished!')
-
+from CCTreeDemonstrator.CCTTreeDemonstratorr import CCTreeDemonstrator
 
 if __name__ == '__main__':
     # set a range of possible attributes which random test data is created from later on
     languages = ['english', 'german', 'french', 'italian', ]
     nr_of_links = range(5)
-    nr_of_words = range(20, 40)
+    nr_of_words = range(30, 40)
     has_non_ascii = [True, False]
 
     data = []
 
     # test data is created randomly
-    for i in range(200):
+    for i in range(10):
         data.append([
             random.choice(languages),
             random.choice(nr_of_links),
@@ -71,7 +23,11 @@ if __name__ == '__main__':
             random.choice(has_non_ascii),
         ])
 
-    tree = CCTree(data, 2, 5)
+    # create CCTree with test data
+    cctree = CCTree(data, 0.5, 2)
+
+    # create Demonstrator object
+    demonstrator = CCTreeDemonstrator(cctree)
 
     # interactively build up the CCTree
-    build_cctree_interactive(tree)
+    demonstrator.build_cctree_interactive()
